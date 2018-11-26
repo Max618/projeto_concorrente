42a9a9f06a0af77b5fda195ac5168f6dee88ba34
Matheus Cândido <matheus9bh@hotmail.com>
2018-11-25 23:48:05 -0200

    add-user-set-files

--- parte3/Usuario.cpp
+++ parte3/Usuario.cpp
@@ -1,9 +1,17 @@
 #include<iostream>
 #include"Usuario.h"
 #include"Arquivo.h"
+#include<string>
 
 using namespace std;
 
-Usuario::Usuario(int numero): arquivos(){
+Usuario::Usuario(int numero, string nome_usuario, int prior): arquivos(){
     this->numero = numero;
+    this->nome = nome_usuario;
+    this->prioridade = prior;
 }
+
+void Usuario::SetArquivos(Arquivo *arq){`
+    this->arquivos = arq;
+}
+
