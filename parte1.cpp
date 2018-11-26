310a81fce5545c8599768dcbbf29bf350cccaee6
Matheus Cândido <matheus9bh@hotmail.com>
2018-11-25 23:08:00 -0200

    rmv-merge-sort

--- parte1.cpp
+++ parte1.cpp
@@ -39,38 +39,7 @@ void insertion_sort(int *elementos, int n){
 	}
 }
 
-void Juntar(int vetor[], int ini, int meio, int fim, int vetAux[]) {
-    int esq = ini;
-    int dir = meio;
-    for (int i = ini; i < fim; ++i) {
-        if ((esq < meio) and ((dir >= fim) or (vetor[esq] < vetor[dir]))) {
-            vetAux[i] = vetor[esq];
-            ++esq;
-        }
-        else {
-            vetAux[i] = vetor[dir];
-            ++dir;
-        }
-    }
-    //copiando o vetor de volta
-    for (int i = ini; i < fim; ++i) {
-        vetor[i] = vetAux[i];
-    }
-}
 
-void MergeSort(int vetor[], int inicio, int fim, int vetorAux[]) {
-    if ((fim - inicio) < 2) return;
-    
-    int meio = ((inicio + fim)/2);
-    MergeSort(vetor, inicio, meio, vetorAux);
-    MergeSort(vetor, meio, fim, vetorAux);
-    Juntar(vetor, inicio, meio, fim, vetorAux);
-}
-
-void merge_sort(int vetor[], int tamanho) {
-    int vetorAux[tamanho];
-    MergeSort(vetor, 0, tamanho, vetorAux);
-}
 
 int main() {
     int TAM = 100000;
